d928022016563ef523aea547c49e4b2ab6fbe9f7
Daria <d.al.tarasova@gmail.com>
2023-09-17 16:07:49 +0200

    error with draggable component fixed

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,14 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
